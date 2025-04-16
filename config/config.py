8ac39ad36db657cabc4998bb48d3aca1cb7cb476
zerosugar6464 <musickumal@gmail.com>
2025-04-16 17:01:40 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/saymen930/Alexa")
 
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
